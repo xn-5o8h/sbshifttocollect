e7e71058c706b673fab09a241d1c5699a43810a7
xn-5o8h <cyelae+gh@vivaldi.net>
2017-12-25 11:53:08 +0100

    Removed redundant items storage, used pcall

--- items/active/unsorted/stctrickeries/stctrickeries.lua
+++ items/active/unsorted/stctrickeries/stctrickeries.lua
@@ -1,48 +1,32 @@
 require "/scripts/util.lua"
 
 function update(_,_, shiftHeld)
-  containerId = config.getParameter("containerId")
-  swapItem = config.getParameter("swapItem")
+  self.containerId = config.getParameter("containerId")
+  self.swapItem = config.getParameter("swapItem")
 
-  if storage.items then
-    if not storage.cantFix then
-      for offset, item in pairs(storage.items) do
-        lootItem(item, offset)
-      end
-    end
-
-    if #storage.items > 0 or storage.cantFix then
-      storage.cantFix = true
-      sb.logInfo("uh oh look like we got a problem; please post this to the Shift To Loot mod page!")
-      error(storage.items)
-      return
-    end
+  if shiftHeld and self.containerId and world.entityExists(self.containerId) then
+    status, result = pcall(eatItems)
   end
 
-  if containerId and world.entityExists(containerId) then
-    if shiftHeld then
-      items = world.containerItems(containerId)
-      storage.items = items
-
-      if items then
-        for offset, item in pairs(items) do
-          lootItem(item, offset)
-        end
-
-        player.cleanupItems()
-      end
-    end
-  end
-
-  if swapItem then player.setSwapSlotItem(swapItem)
+  if self.swapItem then player.setSwapSlotItem(self.swapItem)
   else item.setCount(0) end
+end
+
+function eatItems()
+  items = world.containerItems(self.containerId)
+
+  if items then
+    for offset, item in pairs(items) do
+      lootItem(item, offset)
+    end
+
+    player.cleanupItems()
+  end
 end
 
 function lootItem(item, offset)
   player.giveItem(item)
   if player.hasItem(item, true) then
-    world.containerConsumeAt(containerId, offset - 1, item.count)
+    world.containerConsumeAt(self.containerId, offset - 1, item.count)
   end
-
-  storage.items = util.filter(storage.items, function(i) if i ~= item then return true end end)
 end
